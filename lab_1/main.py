ea4ad2c3acdc47848a000aa1957d850babeeed03
Anzhelika Takhmazova <43497908+L1KASA@users.noreply.github.com>
2023-09-29 21:24:29 +0400

    read-file

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,11 +1,25 @@
-import numpy as np
-
+import numpy as np # для математических вычислений
+import pandas as pd # для графиков
 
 def main():
-    pass
-    
-
-
+    # чтение данных из файла по столбцам
+    file_path = 'lab_1\input.txt'
+    feature_0 = []
+    feature_1 = []
+    feature_2 = []
+    feature_3 = []
+    target = []
+    TT = []
+    with open(file_path, 'r') as file:
+        file.readline() # скипаем первую строку
+        for i in file:
+            first, second, third, fourth, fifth, sixth = i.rstrip().split(',')
+            feature_0.append(first)
+            feature_1.append(second)
+            feature_2.append(third)
+            feature_3.append(fourth)
+            target.append(fifth)
+            TT.append(sixth)
 
 if __name__ == '__main__':
     main()
