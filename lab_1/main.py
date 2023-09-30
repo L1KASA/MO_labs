45263cb191614323da0532326e1e987c03d1e826
Anzhelika Takhmazova <43497908+L1KASA@users.noreply.github.com>
2023-09-30 17:54:18 +0400

    sigmoid

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,9 +1,21 @@
 import numpy as np # для математических вычислений
 import pandas as pd # для графиков и парсинга файла
 
+def sigmoid(p):
+    if (p == 1):
+        return
+    degree = p / (1 - p)
+    return 1 / (1 + np.exp(-1 * degree))
+
+def gradient_descent(x, y, iters = 1000, learning_rate = 0.0001, end = 1e-6):
+    pass
+
 def main():
     file_path = 'lab_1\input.txt'
     data = pd.read_table(file_path, sep=',')
+    print(data)
+    #print(sigmoid(-1))
+
     
 
 if __name__ == '__main__':
